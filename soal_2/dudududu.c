8d7c09bdcbfd15d60e2d61df5a0dbb1842cdca70
Ken <145765887+ItunyaHorse@users.noreply.github.com>
2024-05-01 20:40:20 +0700

    lesgo gesss

---
+++ soal_2/dudududu.c
@@ -0,0 +1,7 @@
+#include <pthread.h> //library thread
+
+int pthread_create(pthread_t *restrict tidp,
+                   const pthread_attr_t *restrict attr,
+                   void *(*start_rtn)(void *),
+                   void *restrict arg);
+
---
+++ soal_4/client/client.c
@@ -0,0 +1,8 @@
+#include <pthread.h> //library thread
+
+int pthread_create(pthread_t *restrict tidp,
+                   const pthread_attr_t *restrict attr,
+                   void *(*start_rtn)(void *),
+                   void *restrict arg);
+
+/* Jika berhasil mengembalikan nilai 0, jika error mengembalikan nilai 1 */
