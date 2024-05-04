2c92655e60063630e44f23745cc8b07ff42f7530
Ken <145765887+ItunyaHorse@users.noreply.github.com>
2024-05-04 23:46:14 +0700

    Action jadi

--- soal_3/actions.c
+++ soal_3/actions.c
@@ -1,8 +1,53 @@
-#include <pthread.h> //library thread
+const char *gap(float jarak) {
+    if (jarak <= 3.5)
+    {
+        return "Gogogo";
+    }
+    else if (jarak <= 10){
+        return "Push";
+    }
+    else{
+        return "Stay out of Trouble";
+    }
+}
 
-int pthread_create(pthread_t *restrict tidp,
-                   const pthread_attr_t *restrict attr,
-                   void *(*start_rtn)(void *),
-                   void *restrict arg);
+const char *fuel(int bensin) {
+    if (bensin < 50)
+    {
+        return "Conserve fuel";
+    }
+    else if (bensin <= 80){
+        return "You can go";
+    }
+    else{
+        return "Push Push Push";
+    }
+}
 
-/* Jika berhasil mengembalikan nilai 0, jika error mengembalikan nilai 1 */
+const char *ban(int sisa) {
+    if (sisa < 30)
+    {
+        return "Box Box Box";
+    }
+    else if (sisa <= 50){
+        return "Conserve Your Tire";
+    }
+    else if (sisa <= 80){
+        return "Good Tire Wear";
+    }
+    else{
+        return "Push Go Push";
+    }
+}
+
+const char *tire(char *tipe) {
+    if (strcmp("Soft", tipe) == 0)
+    {
+        return "Mediums Ready";
+    }
+    else if(strcmp("Medium", tipe) == 0){
+        return "Box for Softs";
+    }
+    
+}
+
