1c26f5132e06d508b2d016cecb055e1bce52ea35
Ken <145765887+ItunyaHorse@users.noreply.github.com>
2024-05-04 23:46:42 +0700

    On progress 80%

--- soal_3/driver.c
+++ soal_3/driver.c
@@ -1,8 +1,61 @@
-#include <pthread.h> //library thread
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <arpa/inet.h>
 
-int pthread_create(pthread_t *restrict tidp,
-                   const pthread_attr_t *restrict attr,
-                   void *(*start_rtn)(void *),
-                   void *restrict arg);
+#define PORT 8080
+#define IP "127.0.0.1"
 
-/* Jika berhasil mengembalikan nilai 0, jika error mengembalikan nilai 1 */
+int main(int argc, char *argv[]) {
+    char *jenis = argv[1];
+    char *jumlah = argv[2];
+
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char buffer[1024];
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    inet_pton(AF_INET, IP, &serv_addr.sin_addr);
+    connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+
+    printf("Koneksi Berhasil\n");
+
+
+    bzero(buffer, 1024);
+    strcpy(buffer, jenis);
+    send(sock, buffer, strlen(buffer), 0);
+
+    if (strcmp("Gap", jenis)== 0)
+    {
+        float angka = atof(jumlah);
+        send(sock, &angka, sizeof(angka), 0);
+    }
+    else if (strcmp("Fuel", jenis)== 0)
+    {
+        int angka = atoi(jumlah);
+        send(sock, &angka, sizeof(angka), 0);
+    }
+    else if (strcmp("Sisa", jenis)== 0)
+    {
+        int angka = atoi(jumlah);
+        send(sock, &angka, sizeof(angka), 0);
+    }
+    else if (strcmp("Tire", jenis)== 0)
+    {
+        if(strcmp("Medium", jumlah)== 0){
+            bzero(buffer, 1024);
+            strcpy(buffer, jumlah);
+            send(sock, buffer, strlen(buffer), 0);
+        }
+        else if (strcmp("Soft", jumlah)== 0){
+            bzero(buffer, 1024);
+            strcpy(buffer, jumlah);
+            send(sock, buffer, strlen(buffer), 0);
+        }
+    }
+    
+}
