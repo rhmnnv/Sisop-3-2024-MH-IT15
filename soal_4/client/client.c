10521fd323ccd5c60b922ddbca62beac268312d5
rhmnnv <150211127+rhmnnv@users.noreply.github.com>
2024-05-06 22:15:49 +0700

    DONE

--- soal_4/client/client.c
+++ soal_4/client/client.c
@@ -1,8 +1,54 @@
-#include <pthread.h> //library thread
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
+#include <arpa/inet.h>
 
-int pthread_create(pthread_t *restrict tidp,
-                   const pthread_attr_t *restrict attr,
-                   void *(*start_rtn)(void *),
-                   void *restrict arg);
+#define PORT 8080
 
-/* Jika berhasil mengembalikan nilai 0, jika error mengembalikan nilai 1 */
+int main(int argc, char const *argv[]) {
+    struct sockaddr_in address;
+    int sock = 0;
+    struct sockaddr_in serv_addr;
+    char command[2048] = {0};
+    char response[4096] = {0};
+
+    if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) {
+        printf("\n Socket creation error \n");
+        return -1;
+    }
+
+    memset(&serv_addr, '0', sizeof(serv_addr));
+
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(PORT);
+
+    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0) {
+        printf("\nInvalid address/ Address not supported \n");
+        return -1;
+    }
+
+    if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) {
+        printf("\nConnection gagal \n");
+        return -1;
+    }
+
+    while (1) {
+        printf("You: ");
+        fgets(command, 2048, stdin);
+        command[strcspn(command, "\n")] = 0;
+
+        send(sock, command, strlen(command), 0);
+
+        read(sock, response, 4096);
+        printf("Server: \n%s\n", response);
+
+        if (strcmp(command, "exit") == 0) {
+            break;
+        }
+    }
+
+    return 0;
+}
